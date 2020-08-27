5241d96424ce60abd2cf3282e03681941179c559
LiuEnTing <4a2g0005@stust.edu.tw>
2020-08-27 23:38:42 +0800

    chat3 清單分割

---
+++ chat3.py
@@ -0,0 +1,11 @@
+#清單分割(時間與名字黏在一起成為一個[])
+lines = []
+with open('3.txt', 'r', encoding='utf-8-sig') as f:
+		for line in f:
+			lines.append(line.strip())
+
+for line in lines:
+	s = line.split(' ')
+	time = s[0][:5]
+	name = s[0][5:]
+	print('聊天時間:', time, '名字:', name)
